686ddd122f745f2fc70c693c46c57ed0a70cab76
ZCJ <zcj@MacBook-Pro.local>
2017-04-09 16:58:23 +0800

    Initial Commit

---
+++ ZCJGifInIntroDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCJGifInIntroDemo
+//
+//  Created by ZCJ on 2017/4/9.
+//  Copyright © 2017年 ZCJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCJGifInIntroDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZCJGifInIntroDemo
+//
+//  Created by ZCJ on 2017/4/9.
+//  Copyright © 2017年 ZCJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
